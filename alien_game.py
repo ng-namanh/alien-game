f4f2ed3120823f04b2d5725dd3b5d466a05c3df7
nna-n5Ember13 <namanh200313@gmail.com>
2023-10-13 21:01:37 +0700

    first commit

---
+++ alien_game.py
@@ -0,0 +1,29 @@
+
+import pygame
+from settings import Settings
+from ship import Ship
+import game_function as gf
+from pygame.sprite import Group
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height ))
+    pygame.display.set_caption("Alien Invasion")
+    
+    ship = Ship(ai_settings, screen)
+    
+    bullets = Group()
+    
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+        
+run_game()
+        
+    
